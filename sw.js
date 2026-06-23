b6c4fff3abc00731eeae1243d01d78f68e856aee
ponhidepon-lang <ponhidepon@gmail.com>
2026-06-24 08:12:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,39 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("n4-app-cache").then((cache) => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "icon.png"
-      ]);
+const CACHE_NAME = "n4-app-cache-v1";
+const FILES_TO_CACHE = [
+  "/n4-app/",
+  "/n4-app/index.html",
+  "/n4-app/manifest.json",
+  "/n4-app/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
